a2bd63e6d998f0d6abf4ca79ec9f7dc8c72ea95b
Davor25793 <roko257993@gmail.com>
2021-03-03 17:17:20 +0100

    changes

---
+++ app.js
@@ -0,0 +1,40 @@
+const buttons = [...document.querySelectorAll('.btn-item')];
+let divs = [...document.querySelectorAll('.img-item')]
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(function(el) {
+  el.addEventListener('click', function(e){
+    e.preventDefault();
+    
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
+
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+
+    const id = button.id;
+    remCurr()
+    button.classList.toggle('btn-active')
+
+    divs.forEach(div => {
+      if(div.classList.contains(id)){
+        div.style.display = 'block';
+      }else{
+        div.style.display = 'none';
+      }
+    })
+  })
+})
+
+function remCurr () {
+  buttons.forEach(button => {
+    button.classList.remove('btn-active')
+  })
+}
+
