e1f6f575ebe43406fe0ae63335b5d1e5401f0b83
Davor25793 <roko257993@gmail.com>
2021-03-04 01:41:06 +0100

    final

--- app.js
+++ app.js
@@ -38,3 +38,10 @@ function remCurr () {
   })
 }
 
+//menu
+const menu = document.querySelector('.menu');
+const list = document.querySelector('.list')
+
+menu.addEventListener('click', () => {
+  list.classList.toggle('active')
+})
